fd6ccc4a0b2c1e56cf46d697c9db307eb29b87c7
Thane Shubaly <shubaly@gmail.com>
2022-10-01 01:48:54 -0300

    Clean up the data.

--- docs/queries.sql
+++ docs/queries.sql
@@ -28,6 +28,7 @@ order by
   [Contributor name]
 ;
 
+--data-right-wing
 select
   lower([Recipient]) as Recipient,
   lower([Political Party of Recipient]) as 'Political Party of Recipient',
@@ -36,7 +37,7 @@ select
 from
   od_cntrbtn_audt_e
 where
-  --   [Fiscal/Election date] >= '2019-01-01'
+  -- [Fiscal/Election date] >= '2018-01-01'
   [Political Party of Recipient] in (
     "People's Party of Canada",
     "Libertarian Party of Canada",
@@ -51,27 +52,9 @@ group by
   lower([Political Party of Recipient]),
   lower([Contributor name])
 order by
-  [Contributor name];
+  random();
 
-select
-  lower([Recipient]) as Recipient,
-  lower([Political Party of Recipient]) as 'Political Party of Recipient',
-  lower([Contributor name]) as 'Contributor name',
-  sum([Monetary amount]) as 'Monetary amount'
-from
-  od_cntrbtn_audt_e
-where
-    [Fiscal/Election date] >= '2018-01-01'
-  and [Contributor Province] in ('NS')
-  and [Contributor name] not like 'Contributions of%'
-group by
-  lower([Recipient]),
-  lower([Political Party of Recipient]),
-  lower([Contributor name])
-order by
-  [Contributor name]
-;
-
+-- data-ns
 select
   lower([Recipient]) as Recipient,
   lower([Political Party of Recipient]) as 'Political Party of Recipient',
@@ -83,10 +66,11 @@ where
   [Fiscal/Election date] >= '2018-01-01'
   and [Contributor Province] in ('NS')
   and [Contributor name] not like 'Contributions of%'
+  and [Monetary amount] > 0
 group by
   lower([Recipient]),
   lower([Political Party of Recipient]),
   lower([Contributor name])
 order by
-  [Contributor name]
+  random()
 ;
